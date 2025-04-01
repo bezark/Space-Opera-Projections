e48ab7f1f5128f21d1cf391fd099c08e3467c9d2
bezark <johnbezark@gmail.com>
2025-04-01 19:05:41 -0400

    joy?

---
+++ space-opera-system/Experiments/accel_stretcher.gd
@@ -0,0 +1,5 @@
+extends CSGBox3D
+
+
+func _on_joycon_accel_changed(accel):
+	size = accel
--- space-opera-system/Experiments/joycon.gd
+++ space-opera-system/Experiments/joycon.gd
@@ -1,4 +1,122 @@
 extends Node
 
-func _input(event: InputEvent) -> void:
-	print(event)
+@export var delay = 10
+
+var current_step = 0
+var udp := PacketPeerUDP.new()
+var server_ip = "127.0.0.1"  # Change this if your server is running on another machine
+var server_port = 26760  # Default DSU protocol port
+var peer
+signal accel_changed
+#func _input(event: InputEvent) -> void:
+	#print(event)
+
+
+func _ready():
+	peer = PacketPeerUDP.new()
+	peer.bind(7879)
+	udp.set_dest_address(server_ip, server_port)
+	
+	# Construct and send the handshake packet
+	var handshake_packet = construct_handshake_packet()
+	udp.put_packet(handshake_packet)
+
+
+func _process(_delta):
+	if peer.get_available_packet_count() > 0:
+		var array_bytes = peer.get_packet()
+		var packet_string = array_bytes.get_string_from_ascii()
+		print("Received message: ", packet_string)
+		
+	current_step +=1
+	if current_step >= delay:
+		current_step = 0
+		if udp.get_available_packet_count() > 0:
+			var packet = udp.get_packet()
+			#var array_bytes = peer.get_packet()
+			#var packet_string = array_bytes.get_string_from_utf32()
+			#print("Received message: ", packet_string)
+			process_received_packet(packet)
+
+
+func _on_osc_server_message_received(address, value, time):
+	print(value)
+	print("Sent handshake packet to register for motion data")
+
+
+	
+func construct_handshake_packet() -> PackedByteArray:
+	var packet = PackedByteArray()
+	
+	# DSUS header
+	packet.append_array("DSUS".to_utf8_buffer()) 
+	
+	# Protocol version (0x03E9 in little-endian)
+	packet.append(0xE9)
+	packet.append(0x03)
+	
+	# Length field (payload length + 4 bytes for CRC, setting this as 16 for simplicity)
+	packet.append(16 & 0xFF)
+	packet.append((16 >> 8) & 0xFF)
+	
+	# Placeholder for CRC32 (4 zeroed bytes, some servers may ignore this)
+	packet.append_array([0x00, 0x00, 0x00, 0x00])
+	
+	# Server ID (set to all 0xFF)
+	packet.append_array([0xFF, 0xFF, 0xFF, 0xFF])
+	
+	# Message type (0x02, 0x00, 0x10, 0x00) - "data" request
+	packet.append_array([0x02, 0x00, 0x10, 0x00])
+	
+	# Registration ID (0 for all controllers)
+	packet.append(0x00)
+	
+	# Slot ID (0 for first controller slot)
+	packet.append(0x00)
+	
+	return packet
+
+func process_received_packet(packet: PackedByteArray):
+	# Motion data should be somewhere in this packet; for now, just print raw data
+	#print("Received packet: ", packet)
+	
+	#if packet.size() < 60:
+		#print("Packet too short, ignoring.")
+		#return
+	#
+	## Extract accelerometer data (X, Y, Z)
+	#var accel_y = bytes_to_int16(packet[38], packet[39])
+	#var accel_x = bytes_to_int16(packet[36], packet[37])
+	#var accel_z = bytes_to_int16(packet[40], packet[41])
+	#
+	## Extract gyroscope data (X, Y, Z)
+	#var gyro_x = bytes_to_int16(packet[48], packet[49])
+	#var gyro_y = bytes_to_int16(packet[50], packet[51])
+	#var gyro_z = bytes_to_int16(packet[52], packet[53])
+	#
+	#print("Accel (X,Y,Z): ", accel_x, ", ", accel_y, ", ", accel_z)
+	#print("Gyro (X,Y,Z): ", gyro_x, ", ", gyro_y, ", ", gyro_z)
+
+	if packet.size() < 100:
+		print("Packet too short, ignoring. Size:", packet.size())
+		return
+
+	# Extract accelerometer data (potential new offsets)
+	var accel_x = bytes_to_int16(packet[82], packet[83])
+	var accel_y = bytes_to_int16(packet[84], packet[85])
+	var accel_z = bytes_to_int16(packet[86], packet[87])
+	
+	# Extract gyroscope data (potential new offsets)
+	var gyro_x = bytes_to_int16(packet[88], packet[89])
+	var gyro_y = bytes_to_int16(packet[90], packet[91])
+	var gyro_z = bytes_to_int16(packet[92], packet[93])
+	#prints(accel_x, accel_y, accel_z, gyro_x,gyro_y,gyro_z)
+
+	#print("Accel (X,Y,Z): ", accel_x / 4096.0, "g, ", accel_y / 4096.0, "g, ", accel_z / 4096.0, "g")
+	#print("Gyro (X,Y,Z): ", gyro_x / 16.4, "°/s, ", gyro_y / 16.4, "°/s, ", gyro_z / 16.4, "°/s")
+	accel_changed.emit(Vector3(accel_x / 4096.0, accel_y / 4096.0, accel_z / 4096.0))
+func bytes_to_int16(high: int, low: int) -> int:
+	var value = (high << 8) | low
+	if value >= 32768:
+		value -= 65536
+	return value
