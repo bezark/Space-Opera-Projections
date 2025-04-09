843a8ecfa816c407e3949bd1db0e15d185a11224
bezark <jdb459@nyu.edu>
2025-04-09 18:03:08 -0400

    simple sync

---
+++ space-opera-system/2Components/DatapadAPI/fetch_game_data.gd
@@ -0,0 +1,30 @@
+extends HTTPRequest
+
+@export var url: String = "localhost:8080"
+@export var game_id: String = "u65YG"
+
+signal game_fetched
+
+
+func _ready() -> void:
+	request_completed.connect(self._http_request_completed)
+
+
+
+
+func get_game():
+	var req_string = str(url, "/api/v1/episodes/", game_id)
+	print(req_string)
+	var error = request(req_string)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+func _http_request_completed(result, response_code, headers, body):
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	var response = json.get_data()
+	# print(response.name)
+	# print(response.societies)
+	# print(response.phases)
+	game_fetched.emit(response)
