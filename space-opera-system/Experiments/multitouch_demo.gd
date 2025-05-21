7754d5f728087a2e4d3227a17b17ba75f831f6a8
bezark <johnbezark@gmail.com>
2025-05-20 20:36:57 -0400

    fiddle

---
+++ space-opera-system/Experiments/multitouch_demo.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _input(event):
+	print(event)
+
+
+func _on_button_pressed():
+	print('wwwww') # Replace with function body.
