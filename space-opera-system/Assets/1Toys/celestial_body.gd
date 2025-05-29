46678c3b6cfad3e67e6cb4517619b32f48edb5b6
bezark <jdb459@nyu.edu>
2025-05-29 16:41:23 -0400

    sorta functional solarsystem

--- space-opera-system/Assets/1Toys/celestial_body.gd
+++ space-opera-system/Assets/1Toys/celestial_body.gd
@@ -2,30 +2,41 @@ extends Node3D
 class_name CelestialBody
 
 @export var title: String
-@export var body : Node3D
-@export var distance :float :
+@export var body: Node3D
+@export var sattelite: CelestialBody = null
+@export var distance: float:
 	set(val):
 		distance = val
-		if body:	
+		if body:
 			body.position.x = distance
-@export var speed: float = 0.5:
-	set(val):
-		offset = rotation.y - val*Time.get_unix_time_from_system()
-		speed = val
-		
-@export var spin :float
 
-var offset = 0.0
+
+@export var speed: float = 0.005:
+	set(val):
+		speed = val
+
+@export var spin: float
+
+
+
 
 func _ready() -> void:
+	var parent = get_parent()
+	if sattelite:
+		var remote_transform = RemoteTransform3D.new()
+		remote_transform.update_rotation = false
+		remote_transform.remote_path=sattelite.get_path()
+		body.add_child(remote_transform)
 	$VBoxContainer/Title.text = title
 	if body:
 		distance = body.position.x
-	offset = rotation.y - speed*Time.get_unix_time_from_system()
 	
+	rotation.y = fmod(speed * Time.get_unix_time_from_system(), TAU)
+
+
 func _process(delta: float) -> void:
-	var angle = fmod(Time.get_unix_time_from_system()*speed+offset, TAU)
-	rotation.y = angle
+	rotate_y(speed*delta)
+	body.rotate_y(spin*delta)
 
 
 func _on_distance_value_changed(value: float) -> void:
@@ -34,6 +45,7 @@ func _on_distance_value_changed(value: float) -> void:
 
 func _on_speed_value_changed(value: float) -> void:
 	speed = value
+
 
 func _on_spin_value_changed(value: float) -> void:
 	spin = value
